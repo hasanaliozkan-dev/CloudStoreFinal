19fef800611c5f8638a752283bd8d13146e715b3
hasanaliozkan-dev <hasanaliozkan-dev@outlook.com>
2022-05-20 23:23:56 +0300

    "Finally DONEEEEE!!"

--- index.php
+++ index.php
@@ -33,8 +33,6 @@ try {
     <title>Cloud Store</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="styles/indexPage.css">
-    <script src="cloud_store.js"></script>
-
 </head>
 <body style="background-image: url('images/bodyback.jpeg')">
 
@@ -83,16 +81,6 @@ try {
 </div>
 
 <div class="basket" id="basket">
-
-
-    <div class="total" id="total">
-        <h4> TOTAL PRICE: </h4>
-        <strong><p style="text-align: center; margin-top: 22px; bottom: 0">50₺</p></strong>
-    </div>
-    <button id="buyButton" disabled type="button"
-            style="background-color: #FFC4FF; float: right;margin-top: 25px;margin-right:25px; height: 30px; width: 70px;border-radius: 25%; border: 0px"
-            onclick="openPayment()"><strong>BUY </strong></button>
-
 
 </div>
 <div class="payment_method" id="payment_method" style="visibility: hidden; border-radius: 10px">
@@ -143,6 +131,7 @@ try {
 
 <script>
     var products = [];
+
     <?php
 
     $filters = array(
@@ -179,7 +168,8 @@ try {
 
     var counter = 0;
     var productLength = <?php echo $productLength ?>;
-
+    var basketpro = [];
+    sessionStorage.setItem("basket",JSON.stringify(basketpro))
     class Cloud {
         constructor(title, desc, quantity, price, image_url, category) {
             this.title = title;
@@ -194,15 +184,14 @@ try {
     }
 
     var productList = [];
-    var basketpro = [];
     for (let i = 0; i < productLength; i++) {
 
         productList.push(new Cloud(products[i][1], products[i][2],
             parseInt(products[i][3]), parseInt(products[i][4]), products[i][5], products[i][6]));
     }
 
-    lengthOfList = productList.length;
 
+    lengthOfList = productList.length;
     function displayProducts() {
 
         for (let i = 0; i < lengthOfList; i++) {
@@ -266,6 +255,147 @@ try {
             document.getElementById("buyButton").disabled = false;
         }
 
+    }
+
+
+
+    function addToBasket(){
+        var parent = event.target.parentElement.parentElement.children;
+        console.log(parent[2])
+        //constructor(title,desc,quantity,price,image_url,category){
+        let image_url = parent[0].src;
+        let title = parent[1].innerHTML;
+        let category = parent[2].innerHTML;
+        let desc = parent[3].innerHTML;
+        let price = parent[4].innerHTML;
+        let quantity = parent[5].innerHTML;
+
+
+        let temp = new Cloud(title,desc,quantity,price,image_url,category);
+
+        basketpro.push(temp)
+        sessionStorage.setItem("basket",JSON.stringify(basketpro))
+        displayBasket()
+
+    }
+
+    basketpro = JSON.parse(sessionStorage.getItem("basket"));
+    function displayBasket(){
+
+        let basketcont = document.getElementById("basket");
+        basketcont.innerHTML = "<div class=\"total\" id=\"total\">\n" +
+            "        <h4> TOTAL PRICE:  </h4>\n" +
+            "        <strong><p style=\"text-align: center; margin-top: 22px; bottom: 0\">"+totalPrice()+" ₺</p></strong>\n" +
+            "    </div>\n" +
+            "    <button id=\"buyButton\"  disabled type=\"button\" style=\"background-color: #FFC4FF; float: right;margin-top: 25px;margin-right:25px; height: 30px; width: 70px;border-radius: 25%; border: 0px\" onclick=\"openPayment()\" > <strong>BUY </strong></button>\n" +
+            "\n" +
+            "\n";
+
+
+        if(basketpro.length>0){
+            document.getElementById("buyButton").disabled = false;
+        }
+        for (let i = 0; i < basketpro.length; i++) {
+            basketcont = document.getElementById("basket");
+            let totall = document.getElementById("total");
+            let basket_element = document.createElement("div");
+            basket_element.classList.add("basket-element");
+            let basket_photo = document.createElement("img");
+
+            basket_photo.src = basketpro[i].image_url;
+            basket_photo.classList.add("basketrow");
+            basket_photo.classList.add("display-horizantally");
+            let productname = document.createElement("h4");
+            productname.classList.add("display-horizantally");
+            let realproductname = document.createTextNode(basketpro[i].title);
+            productname.appendChild(realproductname);
+            let priceproduct = document.createElement("p");
+            priceproduct.classList.add("display-horizantally")
+            let realprice = document.createTextNode(basketpro[i].price);
+            priceproduct.appendChild(realprice)
+            let removebutton = document.createElement("button")
+            removebutton.classList.add("display-horizantally");
+            removebutton.classList.add("cart_button");
+            let minus = document.createTextNode("-");
+            removebutton.onclick = removeFromBasket;
+            removebutton.appendChild(minus);
+            basket_element.appendChild(basket_photo);
+            basket_element.appendChild(productname);
+            basket_element.appendChild(priceproduct);
+            basket_element.appendChild(removebutton);
+            totall.before(basket_element)
+        }
+
+
+
+    }
+
+    function removeFromBasket(){
+        let output=  event.target.parentElement.children;
+        let product_name =output[1].innerHTML;
+        for (let i = 0; i < basketpro.length; i++) {
+            if(basketpro[i].title === product_name){
+                basketpro.splice(i,1);
+                break;
+            }
+        }
+        sessionStorage.setItem("basket",JSON.stringify(basketpro))
+        displayBasket();
+
+    }
+    function totalPrice(){
+        basketpro = JSON.parse(sessionStorage.getItem("basket"));
+        let total = 0;
+        for (let i = 0; i < basketpro.length ; i++) {
+            total += parseInt(basketpro[i].price.split(" ")[1]);
+        }
+        return total;
+
+
+
+
+    }
+
+    function openBasket(){
+
+        if(document.getElementById("basket").style.visibility=== "hidden"){
+            document.getElementById("basket").style.visibility= "visible";
+            displayBasket()
+        }
+        else {
+
+            document.getElementById("basket").style.visibility= "hidden"
+        }
+        totalPrice()
+
+    }
+    function openPayment(){
+        document.getElementById("payment_method").style.visibility = "visible";
+        document.getElementById("basket").style.visibility = "hidden";
+    }
+    function closePayment(){
+        document.getElementById("address").value = "";
+        document.getElementById("cash").checked = false;
+        document.getElementById("creditcard").checked = false;
+        document.getElementById("payment_method").style.visibility = "hidden";
+    }
+    function confirm(){
+        if(document.getElementById("address").value === ""
+            || (document.getElementById("cash").checked === false
+                && document.getElementById("creditcard").checked === false)){
+            alert("Please fill all empty places!!!")
+
+
+        }
+        else{
+            basketpro = []
+            sessionStorage.setItem("basket",JSON.stringify(basketpro));
+            document.getElementById("address").value = "";
+            document.getElementById("cash").checked = false;
+            document.getElementById("creditcard").checked = false;
+            document.getElementById("payment_method").style.visibility = "hidden";
+
+        }
     }
 
     displayProducts()
